7843a801d1a720dfd66d3b13ec13c2b74ff51de3
Gurj Jheeta <gurj.jheeta@gmail.com>
2021-03-25 11:00:12 +0000

    Moved goals into a single file that is rolled multiple times for now.

--- FMTest/Program.cs
+++ FMTest/Program.cs
@@ -6,16 +6,19 @@ namespace FMTest
     class Program
     {
         static void Main(string[] args)
-        {            
-            Console.WriteLine(GetRandomPersonality());
+        {
+            Console.WriteLine("These are your three goals.");
             Console.WriteLine(GetRandomCareerGoal());
+            Console.WriteLine(GetRandomCareerGoal());
+            Console.WriteLine(GetRandomCareerGoal());
+
             Console.WriteLine("These are your three formations.");
             Console.WriteLine(GenerateFormation());
             Console.WriteLine(GenerateFormation());
             Console.WriteLine(GenerateFormation());
 
             Console.WriteLine("\nThese are your three advanced formations.");
-            UseAdvancedFormations(true);
+            AdvancedFormations = true;
             Console.WriteLine(GenerateFormation());
             Console.WriteLine(GenerateFormation());
             Console.WriteLine(GenerateFormation());
--- FMTool/FMTool.cs
+++ FMTool/FMTool.cs
@@ -9,29 +9,13 @@ namespace FMTool
     public static class FMTool
     {
         public static bool AdvancedFormations { get; set; } = false;
-
-        private static List<Personality> _personalities;
-        public static List<Personality> Personalities 
-        {
-            get { return _personalities == null ? LoadPersonalities() : _personalities; }
-            set { _personalities = value; }
-        }
+        public static string CareerGoalLocation { get; set; }
 
         private static List<CareerGoal> _careergoals;
         public static List<CareerGoal> CareerGoals
         {
             get { return _careergoals == null ? LoadCareerGoals() : _careergoals; }
             set { _careergoals = value; }
-        }
-
-        public static List<Personality> LoadPersonalities()
-        {
-            string path = Path.Combine(
-                 System.Environment.CurrentDirectory,
-                 "Data",
-                 "personalities.json");
-            var json = File.ReadAllText(path);
-            return JsonSerializer.Deserialize<List<Personality>>(json);
         }
 
         public static List<CareerGoal> LoadCareerGoals()
@@ -44,21 +28,10 @@ namespace FMTool
             return JsonSerializer.Deserialize<List<CareerGoal>>(json);
         }
 
-        public static Personality GetRandomPersonality()
-        {
-            var roll = new Random().Next(Personalities.Count);
-            return Personalities[roll];
-        }
-
         public static CareerGoal GetRandomCareerGoal()
         {
             var roll = new Random().Next(CareerGoals.Count);
             return CareerGoals[roll];
-        }
-
-        public static void UseAdvancedFormations(bool advanced)
-        {
-            AdvancedFormations = advanced;
         }
 
         public static string GenerateFormation()
