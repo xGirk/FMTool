1a6d848d3b44e7ae2b065ee53a66f55cf7bce3d8
Gurj Jheeta <gurj.jheeta@gmail.com>
2021-08-21 22:49:45 +0100

    Updated ui and added ability to change source of challenges.

--- FMTool/Models/CareerGoal.cs
+++ FMTool/Models/CareerGoal.cs
@@ -5,11 +5,25 @@ using System.Text;
 
 namespace FMTool.Models
 {
-    public record CareerGoal : JsonData
+    public record CareerGoal
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AdvancedDescription { get; set; }
+        public int Difficulty { get; set; }
+        public ChallengeLength Length { get; set; }
+
         public override string ToString()
         {
-            return base.ToString();
+            StringBuilder str = new();
+            str.Append(Name + "\n");
+            str.Append(Description + "\n");
+            if (AdvancedDescription != null && AdvancedDescription != "")
+                str.Append(AdvancedDescription + "\n");
+            str.Append("Difficulty: " + Difficulty + "\n");
+            if (Length != ChallengeLength.None)
+                str.Append(Length + "\n");
+            return str.ToString();
         }
     }
 }
